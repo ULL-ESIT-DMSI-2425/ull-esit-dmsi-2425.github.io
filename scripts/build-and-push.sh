9dc085969517badecb40d4a73c2ba1e70220d5ef
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-09-19 12:16:43 +0100

    new build 2024/2025

--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd ../website && \
+#cd ../website && \
 #git init . && \
 # ➜  website git:(master) git remote -v \
 # origin  git@github.com:ULL-ESIT-DMSI-1920/ull-esit-dmsi-1920.github.io.git (fetch) \
-touch .nojekyll && \
-git add . && \
-git ci -am 'new build 2024/2025'; \
-git push --force origin master
-git push --force dmsi2425 master
+touch .nojekyll;
+git add . ;
+git ci -am 'new build 2024/2025'; 
+git push origin master;
+# git push --force dmsi2425 master
